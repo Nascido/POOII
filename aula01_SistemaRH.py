c9159598939b182b22005b07a870287768db3a1f
Rafael Nascimento <rafaelnasci26@gmail.com>
2023-08-17 16:55:34 -0300

    sistema de RH incompleto

---
+++ aula01_SistemaRH.py
@@ -0,0 +1,29 @@
+
+class Employee:
+    def __init__(self) -> None:
+        pass
+
+    def bonus(self):
+        pass
+ 
+    def month_salary(self):
+        pass
+
+    def extra(self):
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+class Trainee:
+    def __init__(self) -> None:
+        pass
+
+    def bonus (self):
+        pass
+
+    def month_salary(self):
+        pass
+    
+    def __str__(self) -> str:
+        pass

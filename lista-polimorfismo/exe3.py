fb2c958a39c315f3486ccb1df2d3f015bbf047a9
Rafael Nascimento <rafaelnasci26@gmail.com>
2023-08-22 11:28:02 -0300

    exe4 inicio

---
+++ lista-polimorfismo/exe3.py
@@ -0,0 +1,35 @@
+
+class Imovel():
+    def __init__(self, endereco, valor) -> None:
+        self._endereco = endereco
+        self._valor = valor
+
+
+class Novo(Imovel):
+    def __init__(self, endereco, valor) -> None:
+        super().__init__(endereco, valor)
+
+        self._adicional = 50000
+
+    def __str__(self) -> str:
+        return f"Imovel Novo com localização: {self._endereco}. Valor = {self._valor + self._adicional}"
+    
+
+class Velho(Imovel):
+    def __init__(self, endereco, valor) -> None:
+        super().__init__(endereco, valor)
+
+        self._desconto = 0.75
+
+    def __str__(self) -> str:
+        return f"Imovel Novo com localização: {self._endereco}. Valor = {self._valor*self._desconto}"
+
+
+beiramar = Novo("Avenida beira mar", 155000)
+
+mauro = Velho("Mauro Ramos", 100000)
+
+print(beiramar)
+
+print(mauro)
+
---
+++ lista-polimorfismo/exe4.py
@@ -0,0 +1,14 @@
+
+admin = ["Felipe Ramos", 122234567755, "RH", 3344455]
+tec = ["Jose da Cunha", 445555555666, "data", 7788800]
+
+
+class Teste():
+    def __init__(self, admin, tec, ingresso, imovel) -> None:
+        self._admin = admin
+        self._tec = tec
+        self._ingresso = ingresso
+        self._imovel = imovel
+
+    def main(self):
+        pass

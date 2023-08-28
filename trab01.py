19c1ddada65552af9580c3715f23a99b90455c9f
Rafael (Desk-linux) <rafael124715@gmail.com>
2023-08-28 15:34:47 -0300

    Modelo inicial de Aluguel: Trab01

--- trab01.py
+++ trab01.py
@@ -1,4 +1,39 @@
 
 """
 Trabalho 01 :  POO II
+
 """
+
+
+class Veiculos:
+    def __int__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+
+class Carro(Veiculos):
+    pass
+
+
+class Moto(Veiculos):
+    pass
+
+
+class PessoaFisica:
+    def __int__(self):
+        pass
+
+
+class Locatario:
+    def __init__(self):
+        pass
+
+
+class Cliente(PessoaFisica, Locatario):
+    pass
+

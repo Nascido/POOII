b226e699da41c7132fd56ac4c2ddfd0675df0dc8
Rafael (Desk-linux) <rafael124715@gmail.com>
2023-08-28 16:35:31 -0300

    Classes Inseridas: Trab01

--- trab01.py
+++ trab01.py
@@ -5,35 +5,40 @@ Trabalho 01 :  POO II
 """
 
 
-class Veiculos:
-    def __int__(self):
-        pass
+class Veiculo:
+    def __int__(self, modelo, placa, documento, classe="normal"):
+        self._modelo = modelo
+        self._placa = placa
+        self._documento = documento
+        self._classe = classe
+        self._alugado = False
+        self._diaria = 50
 
-    def __str__(self):
-        pass
-
-    def __del__(self):
-        pass
-
-
-class Carro(Veiculos):
-    pass
+    
+class Carro(Veiculo):
+    def __init__(self, assentos = 5) -> None:
+        super().__init__()
+        self._assentos = assentos
 
 
-class Moto(Veiculos):
-    pass
+class Moto(Veiculo):
+    def __init__(self, cilindrada = 150) -> None:
+        super().__init__()
+        self._cilindrada = cilindrada
 
 
 class PessoaFisica:
-    def __int__(self):
-        pass
+    def __int__(self, nome, idade, cpf, cnh):
+        self._nome = nome
+        self._idade = idade
+        self._cpf = cpf
+        self._cnh = cnh
 
 
 class Locatario:
-    def __init__(self):
-        pass
+    def __init__(self, dias):
+        self._dias = dias
 
 
 class Cliente(PessoaFisica, Locatario):
-    pass
-
+  pass

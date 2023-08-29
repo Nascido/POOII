a8bd3603a14926ff0c97cb9b18c3f40800361a5f
Rafael (SAM-linux) <rafael124715@gmail.com>
2023-08-29 11:02:56 -0300

    Atualização de Terça: Trab01

--- trab01.py
+++ trab01.py
@@ -7,19 +7,36 @@ import datetime
 
 
 class Veiculo:
-    def __int__(self, modelo, placa, documento, classe="normal"):
-        self._modelo = modelo
-        self._placa = placa
-        self._documento = documento
-        self._classe = classe
+    def __int__(self):
+        self._modelo = None
+        self._placa = None
+        self._documento = None
+        self._classe = None
+        self._cor = None
         self._alugado = False
         self._diaria = 50
 
+        self.registrarVeiculo()
+
+    def registrarVeiculo(self):
+        print("Por favor insira as informações para o registro do Veículo")
+        self._modelo = input("Modelo do Veículo: ")
+        self._placa = input("Placa: ")
+        self._documento = input("Documentação: ")
+        self._classe = input("Normal ou de luxo? ")
+
+    def setDados(self, data):
+        self._modelo = data[0]
+        self._placa = data[1]
+        self._documento = data[2]
+        self._classe = data[3]
+
     
 class Carro(Veiculo):
-    def __init__(self, assentos=5) -> None:
+    def __init__(self, assentos=5, arcondicionado=True) -> None:
         super().__init__()
         self._assentos = assentos
+        self._arcondicionado = arcondicionado
 
 
 class Moto(Veiculo):
@@ -29,16 +46,46 @@ class Moto(Veiculo):
 
 
 class Cliente:
-    def __init__(self, nome, idade, cpf, vencimentoCNH) -> None:
+    def __init__(self, nome=None, idade=None, cpf=None, vencimentoCNH="26/01/2001") -> None:
         self._nome = nome
         self._idade = idade
         self._cpf = cpf
-        self._vencimentoCNH = vencimentoCNH
+        self._vencimentoCNH = datetime.datetime.strptime(vencimentoCNH, "%d/%m/%y")
+        self._permitido = False
+
+    def registrarCliente(self):
+        print("Por favor insira os dados do cliente locatário")
+        self._nome = input("Nome: ")
+        self._idade = input("Idade: ")
+        self._cpf = input("CPF: ")
+        self._vencimentoCNH = input("Vencimento da CNH (dd/mm/yy): ")
+        self.testarIdade()
+        self.testarValidade()
+
+    def setDados(self, data):
+        self._nome = data[0]
+        self._idade = data[1]
+        self._cpf = data[2]
+        self._vencimentoCNH = data[3]
+
+    def testarValidade(self):
+        today = datetime.datetime.now()
+        diff = (self._vencimentoCNH - today).days
+        if diff < 0:
+            self._permitido = False
+            print("Locatário com CNH vencida!")
+        else:
+            self._permitido = True
 
     def testarIdade(self):
-        if self._idade < 24:
-            print("O locatário deve ter idade superior a 24 anos!")
-            del self
+        if self._idade < 22:
+            print("O locatário deve ter idade superior a 22 anos!")
+        else:
+            self._permitido = True
 
 
-fulano = Cliente("Rafael", 34, 1111111, 5)
+cliente_rafael = Cliente("Rafael", 22, 12471455905, "10/06/24")
+cliente_pedro = Cliente("Pedro", 25, 11112222333, "20/08/27")
+cliente_tobias = Cliente("Tobias", 34, 22222233333, "03/03/25")
+
+cliente_sem_nome = Cliente()

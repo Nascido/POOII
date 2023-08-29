356b7f8e874f464fcf3abafd99da6824b0020459
Rafael (SAM-linux) <rafael124715@gmail.com>
2023-08-29 12:04:09 -0300

    getters e setters da classe Veiculo: Trab01

--- trab01.py
+++ trab01.py
@@ -11,7 +11,6 @@ class Veiculo:
         self._modelo = None
         self._placa = None
         self._documento = None
-        self._classe = None
         self._cor = None
         self._alugado = False
         self._diaria = 50
@@ -23,30 +22,41 @@ class Veiculo:
         self._modelo = input("Modelo do Veículo: ")
         self._placa = input("Placa: ")
         self._documento = input("Documentação: ")
-        self._classe = input("Normal ou de luxo? ")
 
     def setDados(self, data):
         self._modelo = data[0]
         self._placa = data[1]
         self._documento = data[2]
-        self._classe = data[3]
 
-    
+    def setAlugado(self, estado):
+        self._alugado = estado
+
+    def setDiaria(self, valor):
+        self._diaria = valor
+
+    def getAlugado(self):
+        return self._alugado
+
+    def getDiaria(self):
+        return self._diaria
+
 class Carro(Veiculo):
-    def __init__(self, assentos=5, arcondicionado=True) -> None:
+    def __init__(self, assentos=5, arcondicionado=True, classe="normal") -> None:
         super().__init__()
         self._assentos = assentos
         self._arcondicionado = arcondicionado
+        self._classe = classe
 
 
 class Moto(Veiculo):
-    def __init__(self, cilindrada=150) -> None:
+    def __init__(self, cilindrada=150, classe="normal") -> None:
         super().__init__()
         self._cilindrada = cilindrada
+        self._classe = classe
 
-
+    
 class Cliente:
-    def __init__(self, nome=None, idade=None, cpf=None, vencimentoCNH="26/01/2001") -> None:
+    def __init__(self, nome=None, idade=None, cpf=None, vencimentoCNH="26/01/01") -> None:
         self._nome = nome
         self._idade = idade
         self._cpf = cpf
@@ -56,9 +66,10 @@ class Cliente:
     def registrarCliente(self):
         print("Por favor insira os dados do cliente locatário")
         self._nome = input("Nome: ")
-        self._idade = input("Idade: ")
+        self._idade = int(input("Idade: "))
         self._cpf = input("CPF: ")
-        self._vencimentoCNH = input("Vencimento da CNH (dd/mm/yy): ")
+        vencimentoCNH = input("Vencimento da CNH (dd/mm/yy): ")
+        self._vencimentoCNH = datetime.datetime.strptime(vencimentoCNH, "%d/%m/%y")
         self.testarIdade()
         self.testarValidade()
 
@@ -66,7 +77,17 @@ class Cliente:
         self._nome = data[0]
         self._idade = data[1]
         self._cpf = data[2]
-        self._vencimentoCNH = data[3]
+        vencimentoCNH = data[3]
+        self._vencimentoCNH = datetime.datetime.strptime(vencimentoCNH, "%d/%m/%y")
+        self.testarIdade()
+        self.testarValidade()
+
+    def setIdade(self, idade):
+        self._idade = idade
+
+    def setCNH(self, cnh):
+        vencimentoCNH = cnh
+        self._vencimentoCNH = datetime.datetime.strptime(vencimentoCNH, "%d/%m/%y")
 
     def testarValidade(self):
         today = datetime.datetime.now()
@@ -88,4 +109,4 @@ cliente_rafael = Cliente("Rafael", 22, 12471455905, "10/06/24")
 cliente_pedro = Cliente("Pedro", 25, 11112222333, "20/08/27")
 cliente_tobias = Cliente("Tobias", 34, 22222233333, "03/03/25")
 
-cliente_sem_nome = Cliente()
+

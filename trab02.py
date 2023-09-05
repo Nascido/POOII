2af3c31771b3a68b7a837086e884ab34633b1101
Rafael (SAM-linux) <rafael124715@gmail.com>
2023-09-05 14:08:38 -0300

    trab02: classe Deck com métodos gerarDecks, embaralhar e distribuir

--- trab02.py
+++ trab02.py
@@ -5,18 +5,46 @@
     um baralho de cartas para nove jogadores diferentes.
 
 """
+from random import shuffle
 
 
 class Deck:
-    def __init__(self):
+    def __init__(self, decks=1, jokers=False, truco=False):
         self._nipes = ['♠', '♣', '♥', '♦']
         self._num = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        self._normalDeck = []
+        self._deck = []
+        self._joker = jokers
+        self._truco = truco
+        self.gerarDeck(jokers, truco)
 
-    def gerarDeck(self):
+    def gerarDeck(self, jokers, truco):
+        foradoTruco = ['8', '9', '10']
         for nipe in self._nipes:
             for num in self._num:
-                self._normalDeck.append((num, nipe))
+                if truco:
+                    if num not in foradoTruco:
+                        self._deck.append((num, nipe))
+                else:
+                    self._deck.append((num, nipe))
+        if jokers:
+            for i in range(2):
+                self._deck.append("Joker")
+
+    def embaralhar(self):
+        shuffle(self._deck)
+
+    def distribuir(self, players, handsize):
+        tamcards = len(players)*handsize
+        tamdeck = len(self._deck)
+
+        if tamdeck >= tamcards:
+            pass
+        else:
+            print("")
+
+
+
+
 
 class Player:
     pass
@@ -24,4 +52,3 @@ class Player:
 
 class Game:
     pass
-

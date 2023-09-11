f39e035c2960a652970d26da1fea369df1680d39
Rafael (Desk-linux) <rafael124715@gmail.com>
2023-09-11 18:15:55 -0300

    Trab02: Classe Game implementada e não completamente testada

--- trab02.py
+++ trab02.py
@@ -16,6 +16,7 @@ class Deck:
         self._deck = []
         self._joker = joker
         self._tipo = tipo
+        self._numberOfDecks = decks
         for i in range(decks):
             self._gerarDeck()
 
@@ -53,6 +54,15 @@ class Deck:
         else:
             raise ValueError("Número de cartas do deck insuficiente!")
 
+    def getdeck(self):
+        return self._deck
+
+    def get_number_of_cards(self):
+        return len(self._deck)
+
+    def get_number_of_decks(self):
+        return self._numberOfDecks
+
     def __str__(self):
         cartas = len(self._deck)
 
@@ -86,6 +96,89 @@ class Player:
 
     def jogarCarta(self, indexCarta):
         return self._hand.pop(indexCarta)
+    
+    def getname(self):
+        return self._name
 
     def __str__(self):
         return f"{self._name}: {self._hand}"
+
+
+class Game:
+    def __init__(self, jogadores=None, jogo="pife") -> None:
+        self._game = jogo
+        self._players = jogadores
+        self._baralho = None
+
+    def start(self):
+        if self._game == "pife":
+            self.jogarPife()
+            return True
+        
+        else:
+            return False
+
+    def registrarPlayer(self, nome):
+        jogador = Player(nome)
+        self._players.append(jogador)
+
+    def removerPlayer(self, nome):
+        self._players.pop(nome)
+
+    def jogarPife(self):
+        if self._players is None:
+            n = 0
+        else:
+            n = len(self._players)
+
+        ready = False
+        print("#################################################")
+        print(f" {n} jogadores inscritos!\n")
+
+        while not ready:
+            if n < 5:
+                i = 5-n
+                print(f"Necessário adicionar mais {i} no mínino.")
+                while i > 0:
+                    nome = input(" - Nome: ")
+                    self.registrarPlayer(nome)
+                    i -= 1
+
+            elif 5 <= n < 9:
+                adicionar = input(f" Deseja adicionar mais algum? ")
+                if adicionar == 'y':
+                    nome = input(" - Nome: ")
+                    self.registrarPlayer(nome)
+                
+                else:
+                    ready = True
+            
+            elif n == 9:
+                print(f"Número máximo de jogadores, o jogo pode começar!\n")
+                ready = True
+            
+            else:
+                print(f"\nNúmero máximo de jogadores excedido!")
+                i = n-9
+                print("#################################################")
+                print(f"Remova {i} jogador(es) de sua escolha!\n")
+                while n > 9:
+                    for player in self._players:
+                        print(f"{player.getname} - {i}")
+
+                    option = int(input(" - Nome para retirar: "))
+                    self.removerPlayer(self._players[option-1])
+                    n -= 1
+
+            n = len(self._players)
+
+        self._deck = Deck(decks=2)
+        self._deck.distribuir(self._players, 9)
+
+    def getdeck(self):
+        return self._deck.getdeck()
+
+    def __str__(self):
+        n = len(self._players)
+        i = self._deck.get_number_of_decks()
+        return f"Jogo {self._game}: {n} pessoas e {i} deck(s)"

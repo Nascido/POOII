df6f3eddc977ec7d903a0aa1f46b3b1ae6394a56
Rafael Nascimento Siqueira Pinto <rafael124715@gmail.com>
2024-03-20 20:47:04 -0300

    alteração

--- TKinter/main.py
+++ TKinter/main.py
@@ -2,10 +2,4 @@ from tkinter import *
 
 root = Tk()
 
-myLabel00 = Label(root, text="Bem vindo")
-myLabel10 = Label(root, text="Name")
-
-myLabel00.grid(row=0, column=0)
-myLabel10.grid(row=1, column=0)
-
 root.mainloop()
